bc189e5bb968e36432e1bd369cdb6b331a7e0cbc
Jahmal Blanchard <jablancdesigns@gmail.com>
2024-11-09 00:28:53 -0600

    netlify function

--- backend/app.ts
+++ backend/app.ts
@@ -6,6 +6,4 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+export default app;
