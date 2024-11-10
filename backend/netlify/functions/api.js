77092c8eabe9d02ee18ae5db3da07674a51e8084
Jahmal Blanchard <jablancdesigns@gmail.com>
2024-11-10 01:27:37 -0600

    fix api function

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -1,7 +1,33 @@
 import serverless from "serverless-http";
+import express from "express";
 
-const app = require("../../app");
+const app = express();
 
+const schema = buildSchema(`
+  type Query {
+    cars: [Car]
+  }
+  type Car {
+    id: ID,
+    make: String,
+    model: String,
+    year: Int
+  }
+`)
+
+const resolvers = {
+  cars() {
+    return carsData.cars
+  },
+}
+
+app.all(
+  "/api",
+  createHandler({
+    schema: schema,
+    rootValue: resolvers,
+  })
+)
 const handler = serverless(app);
 module.exports.handler = async(event,context)=> {
     const result = await handler(event, context);
