c280a68808f7786f996d72ea8122821095457a5b
Jahmal Blanchard <jablancdesigns@gmail.com>
2024-11-10 01:23:16 -0600

    fix api logic

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -1,5 +1,7 @@
 import serverless from "serverless-http";
-import app from "../../app";
+
+const app = require("../../app");
+
 const handler = serverless(app);
 module.exports.handler = async(event,context)=> {
     const result = await handler(event, context);
